8f4a1c0c4b609e78849174cf7ad09e4bdeb47f18
PabloMLessa <pablolessa@id.uff.br>
2023-06-23 21:02:48 -0300

    Adicionando propriedades e setter

--- POO/conta.py
+++ POO/conta.py
@@ -25,3 +25,19 @@ class Conta:
     def transferir(self, valor, conta_destino):
         self.saca(valor)
         conta_destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
